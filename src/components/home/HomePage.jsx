4c6678ef75da69c14bdda91206734cd048faa1ca
tadeoavs <2302061@utrivieramaya.edu.mx>
2025-03-18 13:24:11 -0500

    Se cambia url imagenes

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -148,7 +148,7 @@ export default function HomePage() {
                                     <CardMedia
                                         component="img"
                                         height="250"
-                                        image="/public/images/R.jpg"
+                                        image="/images/R.jpg"
                                         alt="Marvel"
                                         sx={{ objectFit: "cover" }}
                                     />
@@ -179,7 +179,7 @@ export default function HomePage() {
                                     <CardMedia
                                         component="img"
                                         height="250"
-                                        image="/public/images/j.jpg"
+                                        image="/images/j.jpg"
                                         alt="DC"
                                         sx={{ objectFit: "cover" }}
                                     />
